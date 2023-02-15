a622d9ea3450a28f5072e19cb43251809a62c25a
SerhiyZv <skiev31@gmail.com>
2023-02-15 12:46:53 -0500

    update resolvers.js update mutation resolver function

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,32 +1,30 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { signToken } = require('../utils/auth.js');
 
 const resolvers = {
     Query: {
-        me: async (parent, { user }) => {
-            const foundUser = await User.findOne({
-                $or: [{ _id: user._id }, { username: user.username }],
-            });
+        me: async (parent, args, context) => {
+            if (!context.user)
+                throw new AuthenticationError('You need to be logged in to view saved books');
 
-            if (!foundUser) {
-                return 'Cannot find a user with this id!';
-            }
-
-            return foundUser;
+            return await User.findOne({ _id: context.user._id }).populate(
+                'savedBooks'
+            );
         }
     },
     Mutation: {
-        login: async (parent, { username, email, password }) => {
-            const user = await User.findOne({ $or: [{ username: username }, { email: email }] });
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email: email });
 
-            if (!user) return "Can't find this user";
+            if (!user) throw new AuthenticationError("Can't find this user");
 
             const correctPw = await user.isCorrectPassword(password);
 
-            if (!correctPw) return 'Wrong password!';
+            if (!correctPw) throw new AuthenticationError('Wrong password!');
 
             const token = signToken(user);
-            return ({ token, user });
+            return { token, user };
         },
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username: username, email: email, password: password });
@@ -37,15 +35,16 @@ const resolvers = {
 
             return ({ token, user });
         },
-        saveBook: async (parent, { saveBook }, context) => {
+        saveBook: async (parent, { book }, context) => {
             if (!context.user) throw new AuthenticationError('You need to log in');
 
             try {
                 const updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { savedBooks: saveBook } },
-                { new: true, runValidators: true }
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: book } },
+                    { new: true }
                 );
+                
                 return updatedUser;
             } catch (err) {
                 console.log(err);
